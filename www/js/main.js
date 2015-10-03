bdc49be3ecca4bcd569e8dd11bc99a9581d8524b
Jack Wearden <jack@jackwearden.co.uk>
2015-10-03 20:26:16 +0100

    Magic maps n stuff

--- www/js/main.js
+++ www/js/main.js
@@ -97,13 +97,12 @@ cliques.controller('InviteController', function ($scope, $location, $routeParams
   }
 });
 
-cliques.controller('CliqueController', function ($scope) {
-  $scope.locations = [];
+cliques.controller('CliqueController', function ($scope, ArcGISMap) {
   var pusher = new Pusher('bc1eb4a3db0243829910');
   var channel = pusher.subscribe('private-channel');
 
   channel.bind('client-location', function (data) {
-    $scope.locations.push(location);
+    ArcGISMap.addPoint(data.longitude, data.latitude);
   });
 
   channel.bind('pusher:subscription_succeeded', function() {
@@ -114,6 +113,8 @@ cliques.controller('CliqueController', function ($scope) {
         var lat = loc.coords.latitude;
         var long = loc.coords.longitude;
         var triggered = channel.trigger('client-location', { latitude: lat, longitude: long });
+        ArcGISMap.centerAt(long, lat);
+        ArcGISMap.addPoint(long, lat, '#FF0000');
       }, function locationFail(err) {
         console.log('fail');
         console.error(loc);
@@ -121,3 +122,37 @@ cliques.controller('CliqueController', function ($scope) {
     );
   });
 });
+
+cliques.factory('ArcGISMap', function() {
+  var API = {};
+
+  require([
+    "esri/map",
+    "esri/graphic",
+    "esri/layers/GraphicsLayer",
+    "esri/geometry/Point",
+    "esri/symbols/SimpleMarkerSymbol",
+    "dojo/domReady!"
+  ], function(Map, Graphic, GraphicsLayer, Point, SimpleMarkerSymbol) {
+    var map = new Map("map", {
+      basemap: "topo",
+      center: [-1.8880454000000002,52.4770727], // longitude, latitude
+      zoom: 17
+    });
+
+    API.addPoint = function(long, lat, colour) {
+      var locationLayer = new GraphicsLayer();
+      var point = new Point(long, lat);
+      var symbol = new SimpleMarkerSymbol().setColor(colour || "#1036DE").setSize(14);
+      var graphic = new Graphic(point, symbol);
+      locationLayer.add(graphic);
+      map.addLayer(locationLayer)  // Makes sure that map is loaded
+    };
+
+    API.centerAt = function(long, lat) {
+      map.centerAt(new Point(long, lat));
+    };
+  });
+
+  return API;
+})
